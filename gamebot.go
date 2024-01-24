7440d691aedbc477c577ed54c8526c5fec03236a
adel <65723807+theadell@users.noreply.github.com>
2024-01-24 17:20:54 +0100

    model game state

---
+++ gamebot.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log/slog"
+	"sync"
+
+	"github.com/slack-go/slack"
+)
+
+type GameState int
+
+const (
+	WaitState GameState = iota
+	SetupState
+)
+
+type GameBot struct {
+	apiClient *slack.Client
+	state     GameState
+	players   []string
+	quorum    int
+	messageTs string
+	channelID string
+	mu        *sync.Mutex
+}
+
+func NewGameBot(apiToken string) *GameBot {
+	api := slack.New(apiToken)
+	return &GameBot{
+		apiClient: api,
+		state:     WaitState,
+		players:   make([]string, 0),
+		mu:        &sync.Mutex{},
+	}
+}
+
+func (bot *GameBot) HandleCommand(command slack.SlashCommand) {
+	bot.mu.Lock()
+	defer bot.mu.Unlock()
+
+	switch bot.state {
+	case WaitState:
+		if command.Command == "/kicker" {
+			bot.quorum = 4 // Standard game
+			bot.startSetup(command.UserID)
+		} else if command.Command == "/kicker1v1" {
+			bot.quorum = 2 // 1v1 game
+			bot.startSetup(command.UserID)
+		}
+	case SetupState:
+		bot.sendEphemeral(command.UserID, command.ChannelID, "A game is already being set up.")
+	}
+}
+func (bot *GameBot) startSetup(userID string) {
+	bot.state = SetupState
+	bot.players = []string{userID}
+
+	mention := "<@" + userID + ">"
+	attachment := slack.Attachment{
+		Text:       mention + " hat eine Kickerrunde gestartet! Wer ist dabei?",
+		CallbackID: "game_join",
+		Color:      "#4af030",
+		Actions: []slack.AttachmentAction{
+			{
+				Name:  "join",
+				Text:  "Join",
+				Type:  "button",
+				Value: "join",
+			},
+		},
+	}
+	message := slack.MsgOptionAttachments(attachment)
+	channel, timestamp, err := bot.apiClient.PostMessage(channelID, message)
+	if err != nil {
+		slog.Error("Failed to send message", "error", err)
+		bot.state = WaitState
+		bot.players = make([]string, 0)
+		return
+	}
+	bot.messageTs = timestamp
+	slog.Info("Message send successfully", "channel", channel, "timestamp", timestamp)
+
+}
+
+func (bot *GameBot) HandleJoin(userID string) {
+	bot.mu.Lock()
+	defer bot.mu.Unlock()
+
+	if bot.state != SetupState {
+		return
+	}
+}
+
+func (bot *GameBot) sendEphemeral(userID, channelID, text string) {
+	bot.apiClient.PostEphemeral(channelID, userID, slack.MsgOptionText(text, false))
+}
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"log/slog"
 	"net/http"
 	"os"
@@ -12,79 +10,18 @@ import (
 	"github.com/slack-go/slack"
 )
 
-var token, channelID string
+var token, channelID, signingSecret string
 var apiClient *slack.Client
 
 func main() {
 	token = os.Getenv("KICKBOT_TOKEN")
 	channelID = os.Getenv("KICKBOT_CHANNELID")
-	apiClient = slack.New(token)
-
+	signingSecret = os.Getenv("KICKBOT_SIGNING_SECRET")
+	gameBot := NewGameBot(token)
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.HandleFunc("/commands", handleSlackCommands)
+	r.HandleFunc("/commands", gameBot.handleSlackCommand)
+	r.HandleFunc("/events", gameBot.handleSlackEvent)
 	slog.Info("Server running")
 	http.ListenAndServe("127.0.0.1:3000", r)
-
-}
-
-func handleSlackCommands(w http.ResponseWriter, r *http.Request) {
-	verifier, err := slack.NewSecretsVerifier(r.Header, os.Getenv("KICKBOT_SIGNING_SECRET"))
-	if err != nil {
-		slog.Error("failed to create verifier", "error", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	bodyBytes, err := io.ReadAll(r.Body)
-	if err != nil {
-		slog.Error("Failed to read request body", "error", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	r.Body.Close()
-	if _, err := verifier.Write(bodyBytes); err != nil {
-		slog.Error("Failed to wrote request body to verifier", "error", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if err := verifier.Ensure(); err != nil {
-		slog.Info("Invalid request body", "error", err.Error())
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-	cmd, err := slack.SlashCommandParse(r)
-	if err != nil {
-		slog.Error("Failed to parse slash command", "error", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	switch cmd.Command {
-	case "/matchup":
-		sendLineUpMsg(cmd.UserID)
-	}
-}
-
-func sendLineUpMsg(userId string) {
-	mention := "<@" + userId + ">"
-	attachment := slack.Attachment{
-		Text:       mention + " Started a new round! Who's in?",
-		CallbackID: "game_join",
-		Color:      "#4af030",
-		Actions: []slack.AttachmentAction{
-			{
-				Name:  "join",
-				Text:  "Join",
-				Type:  "button",
-				Value: "join",
-			},
-		},
-	}
-	message := slack.MsgOptionAttachments(attachment)
-	channel, timestamp, err := apiClient.PostMessage(channelID, message)
-	if err != nil {
-		slog.Error("Failed to send message", "error", err)
-	}
-	slog.Info("Message send successfully", "channel", channel, "timestamp", timestamp)
 }
